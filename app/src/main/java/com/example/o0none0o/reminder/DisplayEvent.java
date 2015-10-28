7c48402f9c550d0c2bc61c40693ec6db64572e1d
damitchem <damitchem@gmail.com>
2015-10-27 21:25:14 -0400

    Pre-Iteration 2 Update
    Added event selection on display screen and ability to delete events.

--- app/src/main/java/com/example/o0none0o/reminder/DisplayEvent.java
+++ app/src/main/java/com/example/o0none0o/reminder/DisplayEvent.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
@@ -20,13 +21,46 @@ import java.util.Scanner;
 public class DisplayEvent extends AppCompatActivity {
 
     String test;
+    Event eTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         test = intent.getStringExtra(DisplayActivity.EXTRA_MESSAGE);
+        eTest = getEvent(test);
         setContentView(R.layout.activity_display_event);
+        setText();
+
+
+    }
+
+    public Event getEvent(String search){
+        File list = new File("/data/data/com.example.o0none0o.reminder/files/Reminders.txt");
+        Scanner in = null;
+        Event temp = null;
+        if(list.exists()) {
+            try {
+                in = new Scanner(list);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (in.hasNext()) {
+                try {
+                    String line = in.nextLine();
+                    String[] lineSplit = line.split("~");
+                    String[] searchSplit = search.split("~");
+                    if(searchSplit[0].compareTo(lineSplit[0])==0){
+                        temp = new Event(lineSplit[0],lineSplit[2],lineSplit[1]);
+                    }
+                } catch (NoSuchElementException e) {
+                    e.printStackTrace();
+                }
+            }
+            in.close();
+        }
+
+        return temp;
     }
 
     public void deleteEvent(View view){
@@ -44,10 +78,9 @@ public class DisplayEvent extends AppCompatActivity {
                 writer = new BufferedWriter(new FileWriter(temp));
                 while (in.hasNext()) {
                     String line = in.nextLine();
-                    Toast.makeText(DisplayEvent.this, test, Toast.LENGTH_SHORT).show();
                     String[] testLine = line.split("~");
                     String[] displayed = test.split("~");
-                    if (testLine[0].compareTo(displayed[0])==0) {
+                    if ((testLine[0].trim().compareTo(displayed[0].trim())==0)&&(testLine[1].trim().compareTo(displayed[1].trim())==0)) {
                         continue;
                     }
                     else {
@@ -64,6 +97,15 @@ public class DisplayEvent extends AppCompatActivity {
             e.printStackTrace();
         }
         finished();
+    }
+
+    public void setText(){
+        TextView name = (TextView) findViewById(R.id.eventName);
+        name.setText(eTest.getName());
+        TextView desc = (TextView) findViewById(R.id.eventDesc);
+        desc.setText(eTest.getDescription());
+        TextView end = (TextView) findViewById(R.id.eventEnd);
+        end.setText(eTest.getEnd().getTime().toString());
     }
 
     public void finished(){
