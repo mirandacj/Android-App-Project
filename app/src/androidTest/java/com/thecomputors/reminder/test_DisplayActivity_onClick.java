ddcb74ac4869a032d0be91851f7e37df09ccda45
damitchem <damitchem@gmail.com>
2015-11-24 23:20:04 -0500

    Pre-Iteration 3 Push
    Features:
    Login screen
    Tests

--- app/src/androidTest/java/com/thecomputors/reminder/test_DisplayActivity_onClick.java
+++ app/src/androidTest/java/com/thecomputors/reminder/test_DisplayActivity_onClick.java
@@ -1,7 +1,66 @@
 package com.thecomputors.reminder;
 
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
+import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.Collection;
+
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 /**
  * Created by mitchem on 11/23/2015.
  */
-public class test_DisplayActivity_onClick {
+public class test_DisplayActivity_onClick extends ActivityInstrumentationTestCase2{
+
+    Activity currentActivity;
+
+
+    public test_DisplayActivity_onClick() {
+        super(DisplayActivity.class);
+    }
+
+    public void setUp() throws Exception{
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        super.setUp();
+        getActivity();
+
+    }
+
+    public void testOnClick(){
+        onView(withId(R.id.btnAdd)).perform(click());
+        getActivityInstance();
+        assertTrue(currentActivity.getClass().isAssignableFrom(CreateReminder.class));
+        pressBack();
+
+        onView(withId(R.id.btnSettings)).perform(click());
+        getActivityInstance();
+        assertTrue(currentActivity.getClass().isAssignableFrom(DisplayPref.class));
+        pressBack();
+
+        onView(withId(R.id.btnLeft)).perform(click());
+        onView(withId(R.id.btnRight)).perform(click());
+        onView(withId(R.id.tvDateRange)).check(matches(withText("Today")));
+    }
+
+    public Activity getActivityInstance(){
+        getInstrumentation().runOnMainSync(new Runnable() {
+            public void run() {
+                Collection resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED);
+                if (resumedActivities.iterator().hasNext()){
+                    currentActivity = (Activity)resumedActivities.iterator().next();
+                }
+            }
+        });
+
+        return currentActivity;
+
+    }
 }
--- app/src/androidTest/java/com/thecomputors/reminder/test_DisplayActivity_onContextItemSelected.java
+++ app/src/androidTest/java/com/thecomputors/reminder/test_DisplayActivity_onContextItemSelected.java
@@ -1,7 +1,44 @@
 package com.thecomputors.reminder;
 
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by mitchem on 11/23/2015.
  */
-public class test_DisplayActivity_onContextItemSelected {
+public class test_DisplayActivity_onContextItemSelected extends ActivityInstrumentationTestCase2 {
+
+    public test_DisplayActivity_onContextItemSelected() {
+        super(DisplayActivity.class);
+    }
+
+    public void setUp() throws Exception{
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        super.setUp();
+        getActivity();
+
+    }
+
+    public void testContextItemSelected() throws InterruptedException {
+
+        onView(withId(R.id.btnAdd)).perform(click());
+        onView(withId(R.id.msg_et)).perform(click());
+        onView(withId(R.id.msg_et)).perform(typeText("test"));
+        onView(withId(R.id.btnSubmit)).perform(scrollTo(), click());
+
+        onView(withId(R.id.btnLeft)).perform(click());
+        onView(withId(R.id.btnRight)).perform(click());
+
+        onData(is(instanceOf(android.database.sqlite.SQLiteCursor.class))).atPosition(0).perform(click());
+
+    }
 }
